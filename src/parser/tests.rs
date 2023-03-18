007ab53671e03681a190b87a6c95b9e3a553ac6d
Brandon Woodford <ivegot@norto.dev>
2023-03-18 15:31:44 -0500

    Add more parsing tests for elements

--- src/parser/tests.rs
+++ src/parser/tests.rs
@@ -13,8 +13,9 @@ impl Iterator for TestLexer {
     }
 }
 
+// START: ELEMENT TESTING
 #[test]
-fn test_element() {
+fn test_empty_array_element() {
     let items = VecDeque::from([
        Token::new(JsonKind::Space, 1, " ".to_string()),
        Token::new(JsonKind::BeginArray, 1, " ".to_string()),
@@ -30,3 +31,130 @@ fn test_element() {
 
     parser.start();
 }
+
+#[test]
+fn test_array_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::BeginArray, 1, " ".to_string()),
+       Token::new(JsonKind::BeginObject, 1, " ".to_string()),
+       Token::new(JsonKind::EndObject, 1, " ".to_string()),
+       Token::new(JsonKind::ValueSeperator, 1, " ".to_string()),
+       Token::new(JsonKind::BeginObject, 1, " ".to_string()),
+       Token::new(JsonKind::EndObject, 1, " ".to_string()),
+       Token::new(JsonKind::EndArray, 1, " ".to_string()),
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+#[test]
+fn test_empty_object_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::Space, 1, " ".to_string()),
+       Token::new(JsonKind::BeginObject, 1, " ".to_string()),
+       Token::new(JsonKind::EndObject, 1, " ".to_string()),
+       Token::new(JsonKind::Space, 1, " ".to_string())
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+#[test]
+fn test_number_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::Minus, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+       Token::new(JsonKind::DecimalPoint, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+       Token::new(JsonKind::E, 1, " ".to_string()),
+       Token::new(JsonKind::Plus, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+       Token::new(JsonKind::Digit, 1, " ".to_string()),
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+
+#[test]
+fn test_string_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::StringVal, 1, " ".to_string()),
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+#[test]
+fn test_true_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::True, 1, " ".to_string()),
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+#[test]
+fn test_false_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::False, 1, " ".to_string()),
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+#[test]
+fn test_null_element() {
+    let items = VecDeque::from([
+       Token::new(JsonKind::Null, 1, " ".to_string()),
+    ]);
+
+    let lexer = TestLexer {
+        items
+    };
+
+    let mut parser = Parser::new(Box::new(lexer));
+
+    parser.start();
+}
+
+// END: ELEMENT TESTING
